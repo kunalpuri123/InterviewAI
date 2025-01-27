7653df8fea86839c6f733ee5174363d534b0993c
Kunal Puri <112803004+kunalpuri123@users.noreply.github.com>
2025-01-27 17:23:53 +0530

    Update AudioAnalysisModal .jsx

--- app/dashboard/interview/[interviewId]/start/_components/AudioAnalysisModal .jsx
+++ app/dashboard/interview/[interviewId]/start/_components/AudioAnalysisModal .jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import * as tf from "@tensorflow/tfjs";
 import * as speechCommands from "@tensorflow-models/speech-commands";
 
 const AudioAnalysis = ({ handleAudioFeedback }) => {
